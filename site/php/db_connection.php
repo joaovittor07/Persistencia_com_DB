dca6360312084cb8d98b43a50e14890d4dba96e4
joaovittor07 <119264762+joaovittor07@users.noreply.github.com>
2024-08-17 20:02:30 -0300

    Update db_connection.php

--- site/php/db_connection.php
+++ site/php/db_connection.php
@@ -5,10 +5,8 @@ $password = "";
 $dbname = "livraria";
 $port = 3307;
 
-// Criar a conexão
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Checar a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
